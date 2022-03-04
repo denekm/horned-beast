5ad87773a012ab5e9708867152bd347d5f4882b4
Marta <mrtdeneke@gmail.com>
2022-03-03 16:03:51 -0800

    fixed lab1

--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,8 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <HornedBeast title={'Horned Beast'} description={'Here are some Horned Beasts'}/>
-        <HornedBeast imgUrl={'Horned Beast'} src={'https://cdn.pixabay.com/photo/2013/07/18/15/06/ram-164555_1280.jpg'}/>
+        <HornedBeast title={'UniWhal'} imgUrl={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"}description={'Here is a unicorn and a narwhal nuzzling their horns'}/>
+        <HornedBeast title={'Rhino Family'} imgUrl={"https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'} description={'Here is a parent rhino with two babies"}/>
       </>
     )
   }
