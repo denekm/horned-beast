ec6c98fa540cb28ba5dbf9148bdea19fa55f1eee
Marta <mrtdeneke@gmail.com>
2022-03-08 15:35:21 -0800

    lab2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -8,7 +8,6 @@ class HornedBeast extends React.Component {
       votesOfImages: 0
     }
   }
-
   favoriteImages = () => {
     this.setState({
       votesOfImages: this.state.votesOfImages + 1
@@ -16,11 +15,19 @@ class HornedBeast extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imgUrl} alt={this.props.title} title ={this.props.description}/> 
-        <p>{this.props.description}</p>
-      </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Title>{this.props.title}</Card.Title>
+        <Card.Img
+        src={this.props.imageUrl}
+        alt={this.props.title}
+        title={this.props.description}
+        onClick={this.favoriteImages}/>
+        <Card.Body>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text> 💛 {this.state.votesOfImages}</Card.Text>
+        </Card.Body>
+      </Card>
+  
     )
   }
 }
